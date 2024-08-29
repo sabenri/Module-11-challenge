308763cff3d5cc2598652fc71960fd2bba9c78ae
Sabely Sabely <enriquezsabely@gmail.com>
2024-08-28 21:00:04 -0400

    added the js for new notes

--- Develop/Routes/api.js
+++ Develop/Routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const fs = require ("fs").promises;
 const crypto = require ('crypto');
+const { error } = require("console");
 
 router.get("/api/notes", async(req, res) => {
     try {
@@ -15,3 +16,27 @@ router.get("/api/notes", async(req, res) => {
     }
 })
 
+router.post("/api/notes", async (req, res) => {
+    try {
+        const {title, text} = req.body;
+
+        if (!title || !text) {
+            return res.status(400).json({ error:"Missing title or text"});
+        }
+        const newNote = {
+            title,
+            text,
+            id: crypto.randomUUID()
+        };
+
+        const file = await fs.readFile('./db/db.json','utf-8');
+        const db =JSON.parse(file);
+        db.push(newNote);
+       
+        await fs.writeFile('./db/db.json', JSON.stringify(db));
+        res.json(newNote);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "Note failed to save"});
+    }
+});
