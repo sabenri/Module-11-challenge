dbc1ec2b5c8b6917546fc20f05d3a313f0974241
Sabely Sabely <enriquezsabely@gmail.com>
2024-08-29 19:06:15 -0400

    added the routers to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -4,3 +4,8 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const apiRouter = require('./Routes/api.js');
+const htmlRouter = require('./Routes/html.js');
+
+app.use('/api', apiRouter);
+app.use ('/', htmlRouter);
