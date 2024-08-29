c9dc403597717d364b219e5874c3290c46d8f0b9
Sabely Sabely <enriquezsabely@gmail.com>
2024-08-29 19:15:19 -0400

    added the middleware to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -9,3 +9,12 @@ const htmlRouter = require('./Routes/html.js');
 
 app.use('/api', apiRouter);
 app.use ('/', htmlRouter);
+
+app.use(express.json());
+app.use(express.urlencoded({extened: true}));
+app.use(express.static('public'));
+
+app.use ((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('sorry,something went wrong');
+});
