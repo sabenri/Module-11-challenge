a55785a8a3e7b83d3a1fad9c1c599d2cd665fbeb
Sabely Sabely <enriquezsabely@gmail.com>
2024-08-30 00:15:00 -0400

    fixed some typos and reworked structure of code

--- Develop/Routes/api.js
+++ Develop/Routes/api.js
@@ -1,63 +1,62 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs").promises;
+const crypto = require('crypto');
 
-const fs = require ("fs").promises;
-const crypto = require ('crypto');
-const { error } = require("console");
-
-router.get("/api/notes", async(req, res) => {
+router.get("/notes", async (req, res) => {
     try {
-        const file = await fs.readFile('./db/db.jsion', 'utf8'); 
-        const parsedFile = JSON.parse (file);
+        const file = await fs.readFile('./db/db.json', 'utf8');
+        const parsedFile = JSON.parse(file);
         res.json(parsedFile);
-    } catch(error) {
+    } catch (error) {
         console.error(error);
-        res.status(500).json({error: "unable to read note"});
+        res.status(500).json({ error: "Unable to read notes" });
     }
-})
+});
 
-router.post("/api/notes", async (req, res) => {
+router.post("/notes", async (req, res) => {
     try {
-        const {title, text} = req.body;
+        const { title, text } = req.body;
 
         if (!title || !text) {
-            return res.status(400).json({ error:"Missing title or text"});
+            return res.status(400).json({ error: "Missing title or text" });
         }
+
         const newNote = {
             title,
             text,
             id: crypto.randomUUID()
         };
 
-        const file = await fs.readFile('./db/db.json','utf-8');
-        const db =JSON.parse(file);
+        const file = await fs.readFile('./db/db.json', 'utf-8');
+        const db = JSON.parse(file);
         db.push(newNote);
-       
+
         await fs.writeFile('./db/db.json', JSON.stringify(db));
         res.json(newNote);
     } catch (error) {
         console.error(error);
-        res.status(500).json({error: "Note failed to save"});
+        res.status(500).json({ error: "Note failed to save" });
     }
 });
 
-router.delete("/api/notes/:id", async (req,res) => {
+router.delete("./notes/:id", async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const file = await fs.readFile('./db/db.json', 'utf-8');
         let db = JSON.parse(file);
 
-        const newDb = db.filter(note => note.id !==id); 
+        const newDb = db.filter(note => note.id !== id);
 
-        if (db.lenght === newDb.lenght) {
-            return res.status (404).json ({ error : "Note not found"});
+        if (db.length === newDb.length) {
+            return res.status(404).json({ error: "Note not found" });
         }
-        await fs.writeFile('./db/db.jason', JSON.stringify(newDb));
-        res.json ({message:"Note was deleted"});
-    }
-    catch (error) {
+
+        await fs.writeFile('./db/db.json', JSON.stringify(newDb));
+        res.json({ message: "Note was deleted" });
+    } catch (error) {
         console.error(error);
-        res.status(500).json({error:"unable to delete note"});
+        res.status(500).json({ error: "Unable to delete note" });
     }
 });
 
--- Develop/server.js
+++ Develop/server.js
@@ -7,14 +7,23 @@ const PORT = process.env.PORT || 3001;
 const apiRouter = require('./Routes/api.js');
 const htmlRouter = require('./Routes/html.js');
 
-app.use('/api', apiRouter);
-app.use ('/', htmlRouter);
-
 app.use(express.json());
 app.use(express.urlencoded({extened: true}));
 app.use(express.static('public'));
+
+app.use('/api', apiRouter);
+app.use ('/', htmlRouter);
+
 
 app.use ((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('sorry,something went wrong');
 });
+
+app.use((req, res) =>{
+    res.status(404).send('404: Page Not Found'); 
+});
+
+app.listen(PORT,() =>
+    console.log('Listening at http://localhost:${PORT}')
+)
